31b0a5e068d6c6a3a3cd93c7cb22a9b954b73ae3
ariapn <ariapn@gmail.com>
2020-03-27 17:16:15 -0500

    add el-capitan

--- bin/macosx/el-capitan/contrib/r.rb
+++ bin/macosx/el-capitan/contrib/r.rb
@@ -2,8 +2,8 @@ cask 'r' do
   version '3.6.3'
   sha256 '80f12a85b63b6b45640d1165d06861a84fe11efe4a564e0a755cc313d84cdfb4'
 
-  url "https://giesrbt.github.io/repo-r/bin/macosx/contrib/R-#{version}.pkg"
-  appcast 'https://giesrbt.github.io/repo-r/bin/macosx/contrib/'
+  url "https://giesrbt.github.io/repo-r/bin/macosx/el-capitan/contrib/R-#{version}.pkg"
+  appcast 'https://giesrbt.github.io/repo-r/bin/macosx/el-capitan/contrib/'
   name 'R'
   homepage 'https://www.r-project.org/'
 
